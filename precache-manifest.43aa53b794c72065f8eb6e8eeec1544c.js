018e63bb18a9cf3584c9a1905b5fd7fefe181e6d
Jai Jain <jaisandeepjain@gmail.com>
2019-06-10 22:15:04 +0530

    Updates

---
+++ precache-manifest.43aa53b794c72065f8eb6e8eeec1544c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588b3e320886c5097c85020fc1be7da3",
+    "url": "/en-jai-neer/bits-hyperloop-ui/index.html"
+  },
+  {
+    "revision": "b8ae14f8e7c239b17d4b",
+    "url": "/en-jai-neer/bits-hyperloop-ui/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "00c26706e13855a16bea",
+    "url": "/en-jai-neer/bits-hyperloop-ui/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b8ae14f8e7c239b17d4b",
+    "url": "/en-jai-neer/bits-hyperloop-ui/static/js/2.08f4f3e7.chunk.js"
+  },
+  {
+    "revision": "00c26706e13855a16bea",
+    "url": "/en-jai-neer/bits-hyperloop-ui/static/js/main.5dff8c3f.chunk.js"
+  },
+  {
+    "revision": "277e44653a957638aea0",
+    "url": "/en-jai-neer/bits-hyperloop-ui/static/js/runtime~main.8d3a6ae3.js"
+  }
+]);
